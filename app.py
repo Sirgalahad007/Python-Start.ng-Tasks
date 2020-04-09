1d1056567a96c1b7682d59312caeabb7fabec6c7
Sirgalahad007 <62013694+Sirgalahad007@users.noreply.github.com>
2020-04-09 23:54:34 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+
+def collect_details():
+    first_name = input('Enter your first name: ')
+    last_name = input('Enter your last name: ')
+    email = input('Email address: ')
+    details = (first_name, last_name, email)
+    return details
+
+def generate_password(details):
+    characters = string_ascii_letters
+    length = 9
+    password =
+    generate_password()
+
+    password = string_ascii_letters
